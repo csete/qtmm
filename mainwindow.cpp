b3c99e1ad5b6244966f36927601fb173e0370522
Alexandru Csete <oz9aec@gmail.com>
2011-07-08 23:38:19 +0200

    Add license info to each file.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,23 @@
+/*
+ *      mainwindow.cpp -- Main application window for AFSK1200 demodulator
+ *
+ *      Copyright (C) 2011 Alexandru Csete (oz9aec at gmail.com)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 #include <QDebug>
-//#include <QtMultimedia>
 #include <QAudio>
 #include <QMessageBox>
 #include <QTextStream>
@@ -7,7 +25,7 @@
 #include <QDir>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//#include "multimon/multimon.h"
+
 
 
 MainWindow::MainWindow(QWidget *parent) :
--- multimon/cafsk12.cpp
+++ multimon/cafsk12.cpp
@@ -1,3 +1,25 @@
+/*
+ *      cafsk12.cpp -- AFSK1200 demodulator class
+ *
+ *      Copyright (C) 1996
+ *          Thomas Sailer (sailer@ife.ee.ethz.ch, hb9jnx@hb9w.che.eu)
+ *
+ *      Copyright (C) 2011 Alexandru Csete (oz9aec at gmail.com)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 #include <QDebug>
 #include <QTime>
 #include <math.h>
